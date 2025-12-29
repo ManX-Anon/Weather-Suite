0fafb08dc361b4dd26c8037875ca30a53844a3bd
0xredcrew1 <190328113+ManX-Anon@users.noreply.github.com>
2025-12-29 18:54:27 +0530

    Update settings.py

--- src/config/settings.py
+++ src/config/settings.py
@@ -8,7 +8,7 @@ from typing import Optional
 
 import os
 
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 ROOT_DIR = Path(__file__).resolve().parents[2]
 ENV_FILE = ROOT_DIR / ".env"
